52c01c22c261e5778d582bec1d49e7472d154308
MRAWAISAFTAB <aftabawais36@gmail.com>
2025-05-19 20:41:28 +0500

    calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+var Num1 = +prompt ("Addition \n Enter your 1st number:")
+var Num2 = +prompt ("Enter your 2nd number")
+
+let alert1 = (`Result: \n ${Num1 + Num2}`);
+alert(alert1);
+
+var Num3 = +prompt ("Division \n Now 1st Division Number")
+var Num4 = +prompt ("Now 2nd Division Number")
+
+let alert2 = (`Result: \n ${Num3 / Num4}`);
+alert(alert2);
+
+var Num5 = +prompt ("Square-root \n 1st Number")
+var Num6 = 2
+
+let alert3 = (`Square root number: \n ${Num5 / Num6}`);
+alert(alert3);
+
+var Num7 = +prompt (" Multiplication \n Now Multiply Number")
+var Num8 = +prompt ("Now Multiply Number")
+
+let alert4 = (`Result: \n ${Num7 * Num8}`);
+alert(alert4);
+
+var message = ("Thanks for trying out")
+alert (message)
+
+
